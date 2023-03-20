941dcad3115d49e43430517d43657ef59894581f
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2023-03-20 16:45:57 +0100

    fin sidebar sans scss

--- client/src/components/Header/SideBar.jsx
+++ client/src/components/Header/SideBar.jsx
@@ -14,17 +14,10 @@ export default function SideBar() {
         <SideBarButton name="Accueil" link="youtube.com" />
         <SideBarButton name="Shorts" link="shorts.com" />
         <SideBarButton name="Abonnements" link="Abonnements.com" />
-        <SideBarButton name="Bibliothèque" link="bibliothèque.com" />
         <SideBarButton name="Historique" link="historique.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
-        <SideBarButton name="Shorts" link="shorts.com" />
+        <SideBarButton name="À regarder plus tard" link="loiiiiinnnn.com" />
+        <SideBarButton name="Vidéos J'aime" link="kiffffff.com" />
+       
 
         </div>
 
