c1b95e3fd2083bafe4138769c9df302d1e0a9064
JooBV <kctuus@outlook.fr>
2023-03-20 13:42:16 +0100

    OUIIIII

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/* import './App.css';
+import App from './App'; */
+
+import './usersManagement/Inscription.css';
+import Inscription from './usersManagement/Inscription';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Inscription />
   </React.StrictMode>
 );
 
