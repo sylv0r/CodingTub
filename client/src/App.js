808be9b669a00901f5009ce7216313f4b8bcbaee
BastienVitour <bvitour2004@gmail.com>
2023-03-20 15:54:10 +0100

    init video display branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header'
+import VideoList from './components/VideoList/VideoList';
 
 function App() {
   return (
     <div>
       <Header />
+      <VideoList />
     </div>
   );
 }
---
+++ client/src/components/VideoList/VideoList.jsx
@@ -0,0 +1,13 @@
+import './videolist.scss'
+
+export default function VideoList() {
+
+    return (
+
+        <div id="video-list">
+            <h3>Videos</h3>
+        </div>
+
+    )
+
+}
