72ff8348811ae187627d3f42a5ec0e337e5e1d14
Floria <florian.richet2004@gmail.com>
2023-03-22 11:16:13 +0100

    on avance on avance

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,50 @@
 import './App.css';
 
+
+
+
+
+//////////////////////////LIVE\\\\\\\\\\\\\\\\\\\\\\\
+
+//Configuration des sockets pour relier le serveur au client
+import io from 'socket.io-client';
+const socket = io.connect('http://localhost:3001');
+//Configuration des sockets pour relier le serveur au client
+
+//Configuration des Peer to Peer 
+const pc = new RTCPeerConnection();
+
+// Connection Peer
+navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+  .then(stream => {
+    stream.getTracks().forEach(track => {
+      pc.addTrack(track, stream);
+    });
+
+    // Envoie la requete au serveur
+    pc.createOffer().then(offer => {
+      pc.setLocalDescription(offer);
+      socket.emit('offer', offer);
+    });
+  });
+
+// Reçois la réponse du serveur
+socket.on('answer', answer => {
+  pc.setRemoteDescription(answer);
+});
+
+// Reçois un ICE candidat du serveur
+socket.on('candidate', candidate => {
+  pc.addIceCandidate(candidate);
+});
+
+//////////////////////////LIVE\\\\\\\\\\\\\\\\\\\\\\\
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 3001
+const port = 3005
 const cors = require('cors')
 const mysql = require('mysql');
 const initDb = require('./initDb')
+
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,3 +23,53 @@ app.listen(port, () => {
     console.log("Connected!");
   });
 })
+
+
+
+
+//////////////////////////LIVE\\\\\\\\\\\\\\\\\\\\\\\
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // Réception de l'offre du client
+  socket.on('offer', (offer) => {
+    console.log('Received offer:', offer);
+
+    // Envoi de l'offre au client suivant
+    socket.broadcast.emit('offer', offer);
+  });
+
+  // Réception de la réponse du client suivant
+  socket.on('answer', (answer) => {
+    console.log('Received answer:', answer);
+
+    // Envoi de la réponse au client initial
+    socket.broadcast.emit('answer', answer);
+  });
+
+  // Réception des candidats ICE du client
+  socket.on('candidate', (candidate) => {
+    console.log('Received candidate:', candidate);
+
+    // Envoi des candidats ICE au client suivant
+    socket.broadcast.emit('candidate', candidate);
+  });
+
+  // Gestion de la déconnexion
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+//////////////////////////LIVE\\\\\\\\\\\\\\\\\\\\\\\
