54467e07b1826cda91390da3b0a124b49998a580
Psycockaque <113451954+Psycockaque@users.noreply.github.com>
2023-03-22 11:40:17 +0100

    error

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 //import Search from './components/search/Searchs.js';
 import Filtre from './components/search/filtre.js';
 import Search from './components/search/search.js';
+import React from "react";
+
 
 function App() {
   return (
