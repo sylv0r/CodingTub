9226d745255cdbf22638b4770a6185472aeec9aa
Psycockaque <113451954+Psycockaque@users.noreply.github.com>
2023-03-22 10:42:03 +0100

    rechercheBarre

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+  const express = require('express')
+  const app = express()
+  const bodyParser = require('body-parser')
+  const port = 3001
+  const cors = require('cors')
+  const channelsMiddleware = require('./channel/route')
+
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(cors({ origin: "http://localhost:3000" }));
+
+  //utilise le middleware des channels lorsque la requête commence par /channels
+  app.use("/channels", channelsMiddleware.routes)
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
