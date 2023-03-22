1a7fd136436cc26a1f0069bd2ec7991666d066f7
Hywel <bonnefondhywel@gmail.com>
2023-03-22 10:22:37 +0100

    begin css profile user

--- client/src/components/usersManagement/Affichage/Affichage.js
+++ client/src/components/usersManagement/Affichage/Affichage.js
@@ -7,33 +7,53 @@ function Affichage() {
     const [users, setUsers] = useState([]);
 
     const action = () => {
-        axios.get('http://localhost:3001/users')
-        .then(response => {
+
+        axios.get('http://localhost:3001/users').then(response => {
+
             setUsers(response.data);
             console.log(response.data);
-        })
-        .catch(error => {
+
+        } ).catch(error => {
+
             console.log(error);
-        });
+
+        } );
+
     };
 
     useEffect(() => {
-        action()
-    }, [])
+
+        action();
+
+    }, []);
 
     return (
-        <div>
+
+        <><header className='barUp'></header>
+
+        <div className='barLeft'></div>
+        
+        <div className='user'>
+
             <h1>Users</h1>
+
             {users.map(user => (
-            <ul key={user.id}>
-                <><li>{user.nom}</li>
-                <li>{user.prenom}</li>
-                <li>{user.pseudo}</li>
-                <li>{user.email}</li></>
-            </ul>
+
+                <ul key={user.id}>
+
+                    <li>{user.nom}</li>
+                    <li>{user.prenom}</li>
+                    <li>{user.pseudo}</li>
+                    <li>{user.email}</li>
+
+                </ul>
+
             ))}
-        </div>
+
+        </div></>
+
     );
-}
+
+};
 
 export default Affichage;
