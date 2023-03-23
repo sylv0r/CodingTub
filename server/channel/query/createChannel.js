0825d5890b5c2a9a6c1d74d2c0b68bdbe6314908
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-03-23 17:53:05 +0100

    check extension

--- server/channel/query/createChannel.js
+++ server/channel/query/createChannel.js
@@ -1,21 +1,29 @@
 const { con } = require('../../db/connection')
 const { channelAlreadyExist } = require('../methods/channelAlreadyExist')
+const path = require("path")
 
 module.exports = async (req, res) => {
   const { name, description, image_link, user_id } = req.body
   try {
     if (name.length >= 4 && description.length >= 10) {
       const alreadyExist = await channelAlreadyExist(name)
-      if (!alreadyExist[0]) {
-        await con.query2('INSERT INTO channels (name, description, image_link, user_id) VALUES (?,?,?,?)', [name, description, image_link, user_id]);
-        res.status(201).json({
-          message: "Chaîne créée avec succès"
-        })
+      const extension = path.extname(image_link)
+      if (!extension || (extension == ".jpeg" || extension == ".jpg" || extension == ".png")) {
+        if (!alreadyExist[0]) {
+          await con.query2('INSERT INTO channels (name, description, image_link, user_id) VALUES (?,?,?,?)', [name, description, image_link, user_id]);
+          res.status(201).json({
+            message: "Chaîne créée avec succès"
+          })
+        } else {
+          res.status(406).json({
+            error: "Ce nom de chaîne existe déjà"
+          })
+        }
       } else {
         res.status(406).json({
-          error: "Ce nom de chaîne existe déjà"
+          error: "Votre miniature doit avoir une extension valide"
         })
-      }
+      }  
     } else {
       res.status(406).json({
         error: "Veuillez remplir les champs correctement"
