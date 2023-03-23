21241a7cbd36bc340d8f2573ef6a36907b1ba765
BastienVitour <bvitour2004@gmail.com>
2023-03-23 13:32:38 +0100

    new routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './components/Home';
+import Playlists from './components/Playlists';
+import History from './components/History';
+import Abonnements from './components/Abonnements';
 
 import {
   createBrowserRouter,
@@ -12,6 +15,18 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>
+  },
+  {
+    path: "/playlist",
+    element: <Playlists />
+  },
+  {
+    path: "/history",
+    element: <History />
+  },
+  {
+    path: "/subscriptions",
+    element: <Abonnements />
   }
 ]);
 
