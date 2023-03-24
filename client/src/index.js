ccf2f0efb9bc3a41c76270e7950095a873a57b1c
Floria <florian.richet2004@gmail.com>
2023-03-24 10:29:17 +0100

    pret pour merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,22 @@
+import Live from './components/affichageLive/affichageLive';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/affichageLive/affichageLive';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/live",
+    element: <Live />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
