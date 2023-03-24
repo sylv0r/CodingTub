8fec0d0c62500bfe8d0d926d6a209f4b73b942af
sylv0r <sylvian.vidal95@gmail.com>
2023-03-24 16:10:47 +0100

    del RouterProvider

--- client/src/index.js
+++ client/src/index.js
@@ -79,6 +79,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <RouterProvider router={router} />
   </React.StrictMode>
 );
