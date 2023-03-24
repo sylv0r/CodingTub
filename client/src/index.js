a568701045b1f97a613a9f76c5a9b7060f3a1a7d
raphaelglt <raphael.guillot@edu.esiee-it.fr>
2023-03-24 10:08:40 +0100

    merge lucas done

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import CreateChannel from './components/CreateChannel';
+import UploadForm from './components/chaine_upload/UploadForm';
 
 const router = createBrowserRouter([
   {
     path: "/createChannel",
     element: <CreateChannel />,
   },
+  {
+    path: "/uploadVideo",
+    element: <UploadForm />
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
