52e107fd1e3238d858dff09baa63aacfe056a1ef
Lucastor05 <lucasdeniel05@gmail.com>
2023-03-24 09:10:08 +0100

    upload formdata probleme

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import './App.scss';
-import Home from './components/Home';
 import Upload from './components/chaine_upload/UploadForm';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -8,12 +7,12 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Upload />} />
         <Route path="/upload" element={<Upload />} />
 
 
         {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
-        <Route path="*" element={<Home />} />
+        <Route path="*" element={<Upload />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/chaine_upload/UploadForm.js
+++ client/src/components/chaine_upload/UploadForm.js
@@ -1,20 +1,80 @@
 import React, { useState } from 'react';
-
+//import axios
+import axios from 'axios';
 
 const UploadForm = () => {
+    const [selectedVideo, setSelectedVideo] = useState(null);
+    const [selectedMiniature, setSelectedMiniature] = useState(null);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [tags, setTags] = useState('');
+
+
+
+    const handleVideoInputChange = (event) => {
+        setSelectedVideo(event.target.files[0]);
+    };
+
+    const handleMiniatureInputChange = (event) => {
+        setSelectedMiniature(event.target.files[0]);
+    };
+
+    const handleTitleInputChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleDescriptionInputChange = (event) => {
+        setDescription(event.target.value);
+    };
+
+    const handleTagsInputChange = (event) => {
+        setTags(event.target.value);
+    };
+    // Gestionnaire de soumission du formulaire
+    const handleSubmit = (event) => {
+        const baseURL = "http://localhost:3001/channels/uploadVideo";
+
+
+
+        // Empêcher le comportement par défaut du formulaire
+        event.preventDefault();
+
+        // Création d'un objet FormData pour envoyer les données du formulaire en tant que multipart/form-data
+        const formData = new FormData();
+        formData.append('video', selectedVideo);
+        formData.append('title', title);
+        formData.append('description', description);
+        formData.append('tags', tags);
+        formData.append('miniature', selectedMiniature);
+
+        // Envoi des données du formulaire au serveur
+        try {
+            axios.post(baseURL, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }).then((response) => {
+                console.log(response);
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className='upload_container'>
-            <form action='http://localhost:3001/uploadVideo' className="formUpload">
+            <form onSubmit={handleSubmit} className="formUpload">
                 <div className="videoUpload">
-                    <input type="file" id="fileVideo" accept="video/*" placeholder='video' required />
+                    <input onChange={handleVideoInputChange} type="file" id="fileVideo" accept="video/*" placeholder='video' />
                 </div>
                 <div className="formInfosUpload">
-                    <input type="text" id="titleVideo" placeholder='titre' required/>
-                    <input type="text" id="descriptionVideo" placeholder='description' required/>
-                    <input type="text" id="tagsVideo" placeholder='tags' required/>
+                    <input type="text" id="titleVideo" placeholder='titre' required onChange={handleTitleInputChange} />
+                    <input type="text" id="descriptionVideo" placeholder='description' required onChange={handleDescriptionInputChange} />
+                    <input type="text" id="tagsVideo" placeholder='tags' required onChange={handleTagsInputChange} />
                 </div>
                 <div className="miniatureUpload">
-                    <input type="file" id="fileMiniature" accept="image/*" placeholder='minia' required />
+                    <input onChange={handleMiniatureInputChange} type="file" id="fileMiniature" accept="image/*" placeholder='minia' />
                 </div>
                 <button type="submit">Envoyer</button>
             </form>
