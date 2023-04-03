ba7d824061b95eed6fafc9396d8335bd42a486f5
Bibou <jordan.bouville@edu.esiee-it.com>
2023-04-03 11:42:53 +0200

    erfghj

--- server/users/query/createInscription.js
+++ server/users/query/createInscription.js
@@ -4,7 +4,6 @@ module.exports = async (req, res) => {
   console.log(req.body)
   const { nom, prenom, pseudo, email, password} = req.body
   console.log(req.body)
-  const result = await con.query2('INSERT INTO users (nom, prenom, pseudo, email, password) VALUES (?,?,?,?,?)', [nom, prenom, pseudo, email, password]);
-  console.log(result)
-  res.sendStatus(200)
+  await con.query2('INSERT INTO users (nom, prenom, pseudo, email, password) VALUES (?,?,?,?,?)', [nom, prenom, pseudo, email, password]);
+
 }
