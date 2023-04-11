ebbbb003076c61bd94e8034d0b5bcff49381f295
Julien-QTX <julien.quatravaux@live.fr>
2023-04-11 13:22:06 +0200

    j'ai taffer dessus

--- client/src/components/search/Searchs.js
+++ client/src/components/search/Searchs.js
@@ -1,51 +1,58 @@
 import './Searchs.scss';
 import { useState } from 'react';
-import ControlledCarousel from "./hashtag"
+import Hashtag from "./hashtag"
 
 function Search() {
 
     const data = [/*{chaine}, {profil}, {hashtag}, {video},*/"Chocolat", "Chien", "chat", "Café", "Cafeine"];
 
-    const [value, setvalue] = useState("");
+    const [value, setvalue] = useState("  ");
 
     function handleChange (event) {
         setvalue(event.target.value)
     }
 
-    const url = process.env.REACT_APP_NGINX_LINK;
-
     return (
 
     <header className="body">
 
+        <div className="side"></div>
+
         <nav className="recherche">
             <div className="Navsearch">
 
-                <a className="logo" href="/" target="" rel="">
-                    <img src={url + 'logo/logo_codingtub.png'} alt="Logo CodingTube" className="icon_title" width="100" />
+                <a className="logo" href="#" target="" rel="">
+                    <img src="assets/img_header/Project_title_picture.png" alt="Logo CodingTube" class="icon_title" />  
                 </a>
 
                 <div className="search" action="submit">
-                    <input className="searchBar" placeholder="Rechercher" type="text" value={value} onChange={handleChange}/> 
-                    <button className="lanchsubmit" onClick={() => console.log(value)} >Envoyer</button>
+                    <input className="searchBar" placeholder=" Recherche..." type="text" value={value} onChange={handleChange}/> 
+                    <button className="lanchsubmit" onClick={() => console.log(value)} >envoyer</button>
                 </div>
+                <ul>
+                    {value && 
+                        data
+                        .filter((element) => element.includes(value))
+                        .map((element, index) => <li onClick={() => 
+                        setvalue(element)} key={index}>{element}</li>)
+                    }
+                </ul>
 
                 <div className="connect">
 
                     <a className="not"href="" target="" rel="">
-                        <i class="fa-solid fa-bell fa-2x"></i>
+                        <img src="../../../public/assets/img_header/cloche_notification.png" alt="" class="icon_notification"/>
                     </a>
                     
-                    <a className="pp" href="/connexion" target="" rel="">
-                        <i class="fa-solid fa-user fa-2x"></i>
+                    <a className="pp" href="" target="" rel="">
+                        <img src="../../../public/assets/img_header/Profil_picture.png" alt="" class="icon_profile"/>
                     </a>
                 </div>
                 
             </div>
 
             <div className="Navid">
-
-                <ControlledCarousel/>
+                {Hashtag}
 
             </div>
         </nav>
