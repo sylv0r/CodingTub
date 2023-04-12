2f4c1d5850449e3ca97998cd5b0ad32b02abc8b0
Hywel <bonnefondhywel@gmail.com>
2023-04-12 09:49:58 +0200

    supprimer comm

--- server/users/query/getUserBis.js
+++ server/users/query/getUserBis.js
@@ -1,35 +1,3 @@
-/* const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const port = 3001
-const cors = require('cors')
-const mysql = require('mysql');
-const initDb = require('./initDb')
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cors({ origin: "http://localhost:3000" }));
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-
-  var con = mysql.createConnection(initDb);
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-  });
-
-  app.get('/users', function(req, res) {
-    con.query("SELECT * FROM users", function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
-  });
-
-}) */
-
 const { con } = require('../../db/connection.js');
 
 module.exports = async (req, res) => {
