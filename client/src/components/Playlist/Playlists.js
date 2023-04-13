24b31c36c5e149ff9e0c73e8e2d2eb66432d6303
fabien <fabien.arnal@edu.esiee-it.fr>
2023-04-13 21:58:11 +0200

    update components playlist

--- client/src/components/Playlist/Playlists.js
+++ client/src/components/Playlist/Playlists.js
@@ -1,55 +1,16 @@
 import React from 'react';
 import './../style/Playlist.scss';
+import PreviewPlaylist from './PreviewPlaylist';
+import ListPlaylist from './ListPlaylist';
 
 export default function Playlists(){
-
-    
 
     return(
 
         <div className="container">
 
-
-            <div className='container2'>
-                <div className='preview-videos'>
-                    <div className='display-videos'></div>
-                    <div className='display-text'>
-                        <h2>Vidéos</h2>
-                        <p>User</p>
-                        <p>x vidéos - Mise à jour aujourd'hui</p>
-                    </div>
-                    <div className='display-buttons'>
-                        <input type="button" value="Play" className='display-button' id='button-play'></input>
-                        <input type="button" value="Aléatoire" className='display-button' id='button-random'></input>
-                    </div>
-                </div>
-                <div className="list-videos">
-                    <div class="playlist-container">
-                        <ul class="playlist-list">
-                            <li class="playlist-item">
-                                <a href="#">
-                                <img src="" alt="" />
-                                <div class="playlist-item-info">
-                                    <h3 class="playlist-video-title">Nom de la vidéo</h3>
-                                    <p class="playlist-item-infos"><a href='#'>Nom de la chaine</a> nombre de vues - il y a x heures </p>
-                                    <span class="playlist-item-duration">00:00</span>
-                                </div>
-                                </a>
-                            </li>
-                            <li class="playlist-item">
-                                <a href="#">
-                                <img src="" alt="" />
-                                <div class="playlist-item-info">
-                                    <h3 class="playlist-video-title">Nom de la vidéo</h3>
-                                    <p class="playlist-item-infos"><a href='#'>Nom de la chaine</a> nombre de vues - il y a x heures </p>
-                                    <span class="playlist-item-duration">00:00</span>
-                                </div>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+                <PreviewPlaylist/>
+                <ListPlaylist/>
 
         </div>
     );
