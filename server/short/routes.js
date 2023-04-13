b319b82265a8d36caec68efb380c1a849978b6c4
mounir <mounirxchitour@gmail.com>
2023-04-13 11:15:14 +0200

    lastModif

--- server/short/routes.js
+++ server/short/routes.js
@@ -8,7 +8,7 @@ const fetchVideo = require('../short/query/fetchVideo');
 const router = Router()
 router.use(cors());
 router.post('/video', addVideo)
-router.post('/like', likeVideo)
+router.post('/like/:id', likeVideo)
 router.get('/fetch', fetchVideo)
 
 module.exports.routes = router
