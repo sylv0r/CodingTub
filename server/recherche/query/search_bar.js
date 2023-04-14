e2219eef1c7a3ebdbda9fc238741aaf0e30b2520
Mathéo Guillemin <113523843+VictorSulivan@users.noreply.github.com>
2023-04-14 11:41:12 +0200

    version stable

--- server/recherche/query/search_bar.js
+++ server/recherche/query/search_bar.js
@@ -1,7 +1,47 @@
 const { con } = require('../../db/connection')
 
 module.exports = async (req, res) => {
-await con.query('SELECT videos.title, users.pseudo, lives.title, channels.name FROM videos CROSS JOIN users CROSS JOIN channels CROSS JOIN lives;', function (err, results) {
+  let videoTitles = []
+  let userPseudos = []
+  let liveTitles = []
+  let channelNames = []
+
+  con.query('SELECT title FROM videos;', function (err, results) {
     if (err) throw err;
-    res.send(results)
-})}
+    results.forEach(result => {
+      videoTitles.push(result.title)
+    })
+
+    con.query('SELECT pseudo FROM users;', function (err, results) {
+      if (err) throw err;
+      results.forEach(result => {
+        userPseudos.push(result.pseudo)
+      })
+
+      con.query('SELECT title FROM lives;', function (err, results) {
+        if (err) throw err;
+        results.forEach(result => {
+          liveTitles.push(result.title)
+        })
+
+        con.query('SELECT name FROM channels;', function (err, results) {
+          if (err) throw err;
+          results.forEach(result => {
+            channelNames.push(result.name)
+          })
+
+          let data = []
+          for (let i = 0; i < videoTitles.length; i++) {
+            data.push({
+              videoTitle: videoTitles[i],
+              userPseudo: userPseudos[i],
+              liveTitle: liveTitles[i],
+              channelName: channelNames[i]
+            })
+          }
+          res.send(data)
+        })
+      })
+    })
+  })
+}
