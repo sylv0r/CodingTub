4532629afed43ec382741746a74819641c2fa84a
BastienVitour <bvitour2004@gmail.com>
2023-04-17 09:54:05 +0200

    remove console.log

--- client/src/components/Home/VideoList/VideoList.js
+++ client/src/components/Home/VideoList/VideoList.js
@@ -17,7 +17,7 @@ export default function VideoList({ action }) {
                 return response.json()
             })
             .then((json) => {
-                console.log(json)
+                //console.log(json)
                 setVideos(json)
             })
             .catch(error => {
