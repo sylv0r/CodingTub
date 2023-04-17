73a0a09dbddc86554f2193b422780d759a9d332f
Raphaël <113531071+raphaelglt@users.noreply.github.com>
2023-04-17 14:51:22 +0200

    check connected

--- client/src/components/createChannel/CreateChannel.js
+++ client/src/components/createChannel/CreateChannel.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ChannelForm from './ChannelForm'
 import Alert from 'react-bootstrap/Alert';
 import "./CreateChannel.scss"
+import checkConnection from '../../methods/checkConnection';
 
 export default function Channel() {
   const [alert, setAlert] = useState(false)
   const [message, setMessage] = useState("")
   const [responseType, setResponseType] = useState(null)
+
+  useEffect(async () => {
+    await checkConnection()
+  }, [])
 
   return (
     <div className='main'>
