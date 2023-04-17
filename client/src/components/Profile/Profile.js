9ec31c9f19d45cb4c76f17439a0fa08ca6092142
Hywel <bonnefondhywel@gmail.com>
2023-04-17 13:59:47 +0200

    test

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -8,22 +8,25 @@ export default function Profile({ action }) {
     const localId = localStorage.getItem('user_id');
 
     if (!localId){
+
         window.location.href= '/connexion';
+        
     }
 
     const [users, setUsers] = useState([]);
 
     const getProfile = async () => {
 
-        await fetch(`http://localhost:3001/users/${action}`, {method:"GET", headers: { "Content-Type": "application/json"}})
-        .then(response => {
-            return response.json()
-        })
-        .then((json) => {
-            console.log(json)
-            setUsers(json)
-        })
-        .catch(error => {
+        await fetch(`http://localhost:3001/users/${action}`, {method:"GET", headers: { "Content-Type": "application/json"}}).then(response => {
+
+            return response.json();
+
+        }).then((json) => {
+
+            console.log(json);
+            setUsers(json);
+
+        }).catch(error => {
 
             console.log(error);
 
