fad88980df398d575cb62719709b8ffdd6ee7ad0
Julien-QTX <julien.quatravaux@live.fr>
2023-04-18 15:33:31 +0200

    Search server retrouver

--- server/recherche/route.js
+++ server/recherche/route.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const newSearch = require('./query/search');
 const hashtag = require('./query/hashtag/hashtag');
-const searchBar = require('./query/search_bar');
+const SearchBar = require('./query/search_bar');
 const ResultHashtag = require('./query/hashtag/ResultHashtag');
 
 
@@ -10,7 +10,7 @@ const router = Router()
 //lance la fonction newSearch lorsqu'un post est effecté à /recherche/newSearch
 router.post('/search', newSearch)
 router.get('/hashtag', hashtag)
-router.get('/search_bar', searchBar)
+router.get('/search_bar', SearchBar)
 router.get('/resulthashtag/:id_tag', ResultHashtag)
 
 
