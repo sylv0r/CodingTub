4bd7c361dc072e912f7c740d815823fea75669b8
mounir <mounirxchitour@gmail.com>
2023-04-18 17:47:09 +0200

    uploadMP4Done

--- server/short/routes.js
+++ server/short/routes.js
@@ -3,12 +3,27 @@ const cors = require("cors");
 const addVideo = require('../short/query/addVideo');
 const likeVideo = require('../short/query/likeVideo');
 const fetchVideo = require('../short/query/fetchVideo');
-const  addComment  = require('../short/query/commentVideo'); 
-const  fetchComments = require('../short/query/fetchComment')
+const addComment = require('../short/query/commentVideo'); 
+const fetchComments = require('../short/query/fetchComment')
+
+const multer = require("multer");
+
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 const router = Router()
 router.use(cors());
-router.post('/video', addVideo)
+router.post('/video', upload.single("video"), addVideo) 
 router.post('/like/:id', likeVideo)
 router.get('/fetch', fetchVideo)
 router.post('/setcomment/:id', addComment) 
