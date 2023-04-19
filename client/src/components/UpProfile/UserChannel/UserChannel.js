c3ad054c3a229f83bc4705ae6d22c2fb8ebb1968
Alexandre <116552630+Creepynax@users.noreply.github.com>
2023-04-19 15:18:17 +0200

    merge branche Hywel à Alex

--- client/src/components/UpProfile/UserChannel/UserChannel.js
+++ client/src/components/UpProfile/UserChannel/UserChannel.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './UserChannel.scss';
-import axios from 'axios';
-import profilePhoto from './chat.jpg';
 
-export default function UserChannel({ action, name }) {
+export default function UserChannel({ action }) {
 
     // Déclare la variable local pour la connexion
 
@@ -27,32 +25,8 @@ export default function UserChannel({ action, name }) {
     };
 
     // Déclare les variables
-    const [users, setUser] = useState([]);
     const [buttonText, setButtonText] = useState(localStorage.getItem("buttonText") || "S'abonner");
     const [buttonColor, setButtonColor] = useState(localStorage.getItem("buttonColor") || "black");
-
-    //  Afficher les profils en les récupérant dans la base de données
-
-    const getUserChannel = async () => {
-        var currentUrl = window.location.href
-        var split = currentUrl.split('/')
-        const nom = split[split.length-1];
-        
-        try {
-            const reponse = await axios.post(`http://localhost:3001/users/getChannel/${nom}`)
-            setUser(reponse.data[0]);
-        } catch (e) {
-            console.log(e)
-        }
-    };
-
-    function userProfileSub () {
-        getUserChannel();
-    }
-
-    useEffect(() => {
-        userProfileSub();
-    }, []);
 
     // Concerne le bouton "s'abonner"
 
@@ -73,13 +47,13 @@ export default function UserChannel({ action, name }) {
     return (
         <div className='wrapperUserProfile'>
             <div className='profileUserBis'>
-                <img id='pictureUserBis' src={users.image_link} alt='profile' />
+                <img id='pictureUserBis' src={process.env.REACT_APP_NGINX_LINK + action.imageLink.image_link} alt='profile' />
             </div>
             <div className='profileUserBis'>
                 {/* Récupère les données de la base de données grâce à la requête SQL */}
-                <p className='pseudoUsername'>{users.name}</p>
-                <p>{users.subscribers} abonnés {users.number_videos} vidéos</p>
-                <p>{users.description_channel}</p>
+                <p className='pseudoUsername'>{action.name}</p>
+                <p>{action.subscribersChannel.subscribers} abonnés {action.nbVideosChannel.number_videos} vidéos</p>
+                <p>{action.descriptionChannel.description_channel}</p>
             </div>
             <div className='profileUserBis'>
                 <button className='updateUserProfile' type='submit' onClick={subscribed} style={{ backgroundColor: buttonColor }}>
