25d1f2300f38c9c87a967c8f9bf93f1d96f295b8
Hywel <bonnefondhywel@gmail.com>
2023-04-19 11:22:06 +0200

    test

--- server/users/query/getChannel.js
+++ server/users/query/getChannel.js
@@ -2,7 +2,7 @@ const { con } = require('../../db/connection.js');
 
 module.exports = async (req, res) => {
     const name = req.params.name;
-    await con.query2('SELECT * FROM channels WHERE channels.name = ?', [name], function (err, results){ 
+    await con.query('SELECT * FROM channels WHERE channels.name = ?', [name], function (err, results){ 
         if (err) throw err;
         res.send(results)
     })
