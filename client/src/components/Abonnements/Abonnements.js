e5a0d6f2395a1c6c4777e8d3a87a365c5c53ac1f
BastienVitour <bvitour2004@gmail.com>
2023-04-19 15:46:13 +0200

    updated sub and align right vids

--- client/src/components/Abonnements/Abonnements.js
+++ client/src/components/Abonnements/Abonnements.js
@@ -1,15 +1,25 @@
 
 import VideoList from '../Home/VideoList/VideoList';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function Abonnements() {
 
-    let user = localStorage.getItem('user_id')
+  const [user, setUser] = useState(null);
 
-  return (
-    <div>
-      <VideoList action={`getSubscriptionsVideos/${user}`} />
-    </div>
-  );
+
+	useEffect(() => {
+		axios.post('http://localhost:3001/users/getUserId', {
+			hashedUserId : JSON.parse(localStorage.getItem('hashed_user_id'))
+			})
+			.then(async (response) => {
+			setUser(response.data)
+			})
+	}, [])
+	
+	return (
+		user !== null ? <VideoList action={`getSubscriptionsVideos/${user}`} /> : null
+	);
 }
 
 export default Abonnements;
