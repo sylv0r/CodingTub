189809c3577296e53d48f548e8ec74fe0ac0c6b3
Julien-QTX <julien.quatravaux@live.fr>
2023-04-19 14:57:55 +0200

    i don't know :)

--- client/src/components/video/Video.js
+++ client/src/components/video/Video.js
@@ -3,15 +3,15 @@ import ReactPlayer from 'react-player'
 import './video.scss';
 import './listDroite.scss';
 
-import { useSearchParams  } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import VideoListRight from './VideoListRight';
 import Likes from './likes/Likes';
 function Video() {
-    
-    
+
+
     let [searchParams, setSearchParams] = useSearchParams();
     const id = searchParams.get('id'); // send
-    
+
 
 
 
@@ -24,13 +24,13 @@ function Video() {
             method: "GET",
             headers: { 'Content-Type': 'application/json' },
         })
-    
+
         const data = await response.json();
         setVideos(data);
         console.log(data);
     }
     async function handleLikeClick() {
-  
+
 
         await fetch(`http://localhost:3001/videos/likes/`, {
             method: "POST",
@@ -38,7 +38,7 @@ function Video() {
             body: JSON.stringify({
                 id_video: id
             })
-            
+
         });
     }
 
@@ -46,78 +46,69 @@ function Video() {
     const [isActive, setIsActive] = useState(false);
 
     const toggleActive = () => {
-      setIsActive(!isActive);
+        setIsActive(!isActive);
     }
 
     const url = process.env.REACT_APP_NGINX_LINK;
 
-    
 
-    useEffect(()=>{
+
+    useEffect(() => {
         getVideosInfo();
-    }, []) 
+    }, [])
 
 
-    
+
     return (
-                 
+
         <div id='player-wrapper'>
             {videos.length > 0 &&
-            <div className='video'>
-                <ReactPlayer 
-                    url={url + videos[0].video_link}
-                    controls
-                    className='player'
-                    width='50%'
-                    height='50%'
+                <div className='video'>
+                    <ReactPlayer
+                        url={url + videos[0].video_link}
+                        controls
+                        className='player'
+                        width='50%'
+                        height='50%'
                     />
-                    </div>
+                </div>
             }
             {videos.length > 0 &&
-                    
+
                 <div id='video_info'>
                     <h2 id='title_video'>
                         {videos[0].title}
                     </h2>
-                        <div id='video_stats'>
-                            <Likes video={videos} />
-                            <span id='vues'>
+                    <div id='video_stats'>
+                        <Likes video={videos} />
+                        <span id='vues'>
                             {videos[0].vues} vues
-                            </span>
-                        </div>
+                        </span>
+                    </div>
 
-                        <div id='video-description'>
-                            <p>
-                                description :
-                            </p>
-                            <p id='description'>
-                                {videos[0].description}
-                            </p>
-                        </div>
-                        <div id='published'>
-                            <span>
-                                {videos[0].published_at}
-                            </span>
-                        </div>
-                        </div>
+                    <div id='video-description'>
+                        <p>
+                            description :
+                        </p>
+                        <p id='description'>
+                            {videos[0].description}
+                        </p>
+                    </div>
+                    <div id='published'>
+                        <span>
+                            {videos[0].published_at}
+                        </span>
+                    </div>
+                </div>
             }
 
             <VideoListRight />
 
-
-                
-            
         </div>
 
-       
-        
-
-             
     );
-  
-        
-    
-}    
+
+}
 
 
 export default Video;
