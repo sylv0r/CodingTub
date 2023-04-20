d711b7bf599b1591800e64c92844cc641a168200
Lucastor05 <lucasdeniel05@gmail.com>
2023-04-20 12:02:10 +0200

    merge alexandre

--- client/src/index.js
+++ client/src/index.js
@@ -62,13 +62,6 @@ const router = createBrowserRouter([
     element: <ModifyProfile />
   },
   {
-<<<<<<< HEAD
-    path: "/uploadVideo",
-    element: <UploadVideo />
-  },
-  {
-=======
->>>>>>> origin/chaine_alexandre
     path: "/video",
     element: <Video />,
   },
