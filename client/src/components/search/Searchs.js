e102a7e01c0125404b2356f3a9121bbe0cb6ae9c
Raphaël <113531071+raphaelglt@users.noreply.github.com>
2023-04-20 15:51:29 +0200

    ratio alexandre devouard

--- client/src/components/search/Searchs.js
+++ client/src/components/search/Searchs.js
@@ -53,11 +53,16 @@ function Search() {
                     </a>
 
                     {localStorage.getItem("jwt") ?
-                        <a className="pp" href="/modifyProfile" target="" rel="">
-                            <i className="fa-solid fa-user fa-2x"></i>
-                        </a>
+                        <div className='connected-icons'>
+                            <a className="pp" href="/createChannel" target="" rel="">
+                                <i class="fa-solid fa-plus fa-2x"></i>
+                            </a>
+                            <a className="pp" href="/modifyProfile" target="" rel="">
+                                <i className="fa-solid fa-user fa-2x"></i>
+                            </a>
+                        </div>
                     : 
-                    <a className="pp" href="/connexion" target="" rel="">
+                        <a className="pp" href="/connexion" target="" rel="">
                             <i className="fa-solid fa-user fa-2x"></i>
                         </a>
                     }
